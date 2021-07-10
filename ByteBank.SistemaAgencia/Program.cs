996b3e120abfd1d8ad0288c9dd4bef5ea3c978ed
Joao Reis <joaovictorcruzereis@gmail.com>
2021-07-10 18:37:31 -0300

    Adicionar arquivos de projeto.

---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,30 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using Humanizer;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dataFimPagamento = new DateTime(2018, 8, 17);
+            
+            DateTime dataCorrente = DateTime.Now;
+
+            TimeSpan diferenca = TimeSpan.FromMinutes(40);
+
+            string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+
+            Console.WriteLine(dataFimPagamento);
+            Console.WriteLine(dataCorrente);
+            Console.WriteLine(mensagem);
+
+            Console.ReadLine();
+        }
+    }
+}
